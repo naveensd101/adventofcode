027d3c7b757f37d51ac95c6898a32fb50b42612c
nsd <nsd@atlassian.com>
2024-12-06 23:58:54 +0530

    6b edit

--- 2024/6/b/main.py
+++ 2024/6/b/main.py
@@ -5,6 +5,10 @@ def findGirl(map):
     for i, line in enumerate(map):
         for j, ch in enumerate(line):
             if ch == '^': return i, j
+def printDic(dic):
+    for key in dic:
+        print(key, end=" : ")
+        print(dic[key])
 def isGirlAlive(i, j, N):
     if i < 0 : return False
     if j < 0 : return False
@@ -18,11 +22,34 @@ def whereShouldGirlMoveTo(i, j, ch):
     if ch == 'v': return i+1, j
     if ch == '>': return i, j+1
     if ch == '<': return i, j-1
+
+    print("you fucked up inside whereShouldGirlMoveTo : {i}, {j}")
 def rotateArrow(ch):
     if ch == '^': return '>'
     if ch == '>': return 'v'
     if ch == 'v': return '<'
     if ch == '<': return '^'
+def getHistory(map, N):
+    from collections import defaultdict
+    history = defaultdict(list)
+    i, j = findGirl(map)
+    while isGirlAlive(i, j, N):
+        I, J = whereShouldGirlMoveTo(i, j, map[i][j])
+        if not(isGirlAlive(I, J, N)):
+            return history
+
+        if map[i][j] in ['^', '>', 'v', '<']:
+            history[(i,j)].append(map[i][j])
+        if map[I][J] == '#':
+            tmp = map[i][j]
+            map[i][j] = rotateArrow(map[i][j])
+            oi, oj = i, j
+            i, j = whereShouldGirlMoveTo(i, j, map[i][j])
+            map[i][j] = map[oi][oj]
+            map[oi][oj] = tmp
+        else:
+            map[I][J] = map[i][j]
+            i, j = I, J
 def willThisPositionMakeALoop(_i, _j, map, N):
     history = {}
     if map[_i][_j] == '.': map[_i][_j] = '#'
@@ -52,14 +79,9 @@ def willThisPositionMakeALoop(_i, _j, map, N):
             map[I][J] = map[i][j]
             i, j = I, J
 
-# N = 10
-N = 130
+N = 10
+# N = 130
 map = [input() for _ in range(N)]
 map = [list(line) for line in map]
-from copy import deepcopy
-#willThisPositionMakeALoop(0, 0, map)
-ans = 0
-for i in range(N):
-    for j in range(N):
-        if willThisPositionMakeALoop(i, j, deepcopy(map), N): ans += 1
-print(ans)
+history = getHistory(map, N)
+printDic(history)
